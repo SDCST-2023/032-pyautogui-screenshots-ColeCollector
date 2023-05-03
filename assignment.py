c005a61d3ed6a30a0ea692bd325494ff133a39f7
ColeCollector <colen827@deltalearns.ca>
2023-05-03 11:45:13 -0700

    aiuwgdawegi

--- assignment.py
+++ assignment.py
@@ -1,5 +1,6 @@
 import pyautogui as p
 import time
+import random
 import upgrades
 
 #waits 2 seconds
@@ -10,7 +11,7 @@ location = p.locateOnScreen('assets/cookie.png')
 store = p.locateOnScreen('assets/store.png')
 
 
-#Gives the program a lifetime of 10 seconds
+#Gives the program a lifetime of 15 seconds
 start = time.time()
 end = (start + 15)
 
@@ -23,20 +24,25 @@ while end > start:
         golden = p.locateOnScreen('assets/golden.png', confidence = 0.8)
         upgrade = p.locateOnScreen('assets/upgrade.png', confidence = 0.5)
         
-        for i in range (0,100):
+        for i in range (0,150):
             #autoclicker
             p.moveTo(location) 
             p.tripleClick()
 
             #golden cookie finder
-            if golden != None:
-                p.moveTo(golden)
-                p.leftClick
-                print("found a golden cookie")
+            #if golden != None:
+            #   p.moveTo(golden)
+            #    p.leftClick
+            #    print("found a golden cookie")
         
-        #store perks
-        upgrades.perks()
-        upgrades.upgrades()
+        #using the store
+        randomchoice = random.randint(1,2)
+        
+        if randomchoice == 1:
+            upgrades.perks()
+
+        else:
+            upgrades.upgrades()
 
     elif location == None:
         break
--- upgrades.py
+++ upgrades.py
@@ -1,9 +1,11 @@
 import pyautogui as p
 import time
 time.sleep(2)
+
 def perks():
     p.moveTo(1275,500)
     p.scroll(900)
+    time.sleep(0.5)
     store = p.locateCenterOnScreen('assets/store.png')
 
     if store != None:
@@ -13,10 +15,30 @@ def perks():
 
 
 def upgrades():
+    maximum = 0
     p.moveTo(1275,500)
     p.scroll(900)
     time.sleep(0.5)
-    p.scroll(-250)
+    p.scroll(-500)
+    time.sleep(0.5)
+    p.moveTo(1275,50)
+
+    upgradeList = ['wizard','temple','bank','factory','mine','farm','grandma','cursor']
+    
+    for i in upgradeList:
+        buy = p.locateCenterOnScreen(f"assets/{i}.png")
+
+        if buy != None:
+            #buys 3 most expensive items it can
+            if maximum >= 3:
+                break
+
+            else:
+                p.moveTo(buy)
+                p.click()
+                maximum = maximum + 1
+                time.sleep(0.5)
+
 
 
 
