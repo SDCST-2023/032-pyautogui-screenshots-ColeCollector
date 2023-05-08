939e01dd358c7fbbb36d2b4ddd3992b990421c6c
ColeCollector <colen827@deltalearns.ca>
2023-05-08 11:19:34 -0700

    a

--- assignment.py
+++ assignment.py
@@ -3,6 +3,7 @@ import time
 import random
 import sys
 import upgrades
+import golden
 
 #waits 2 seconds
 time.sleep(2)
@@ -22,26 +23,12 @@ while end > start:
 
     #if cookie is found start program
     if location != None:
-        golden1 = p.locateOnScreen('assets/golden1.png', confidence = 0.4)
-        golden2 = p.locateOnScreen('assets/golden2.png', confidence = 0.4)
         
         for i in range (0,300):
             #autoclicker
             p.moveTo(location) 
             p.tripleClick()
-
-            #golden cookie finder
-            if golden1 != None:
-                p.moveTo(golden1)
-                p.click()
-                time.sleep(0.5)
-                print("golden cookie1 found")
-
-            if golden2 != None:
-                p.moveTo(golden2)
-                p.click()
-                time.sleep(0.5)
-                print("golden cookie2 found")
+            golden.golden()
 
         
         #using the store
