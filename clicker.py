8e7b1b929903d8e0a0e3080e06b4b47612b5ea2c
ColeCollector <colen827@deltalearns.ca>
2023-05-09 11:20:44 -0700

    done done done

--- clicker.py
+++ clicker.py
@@ -2,7 +2,7 @@ import pyautogui as p
 import time
 
 def autoclicker():
-    #clicks 15 times
+    #clicks 10 times
     p.click()
     time.sleep(0.01)
     p.click()
@@ -22,15 +22,6 @@ def autoclicker():
     p.click()
     time.sleep(0.01)
     p.click()
-    time.sleep(0.01)
-    p.click()
-    time.sleep(0.01)
-    p.click()
-    time.sleep(0.01)
-    p.click()
-    time.sleep(0.01)
-    p.click()
-    time.sleep(0.01)
-    p.click()
+    
     
     
