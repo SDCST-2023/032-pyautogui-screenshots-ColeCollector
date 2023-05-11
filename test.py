f405d7bd74d5b85da036b7c20563acab989c286b
ColeCollector <colen827@deltalearns.ca>
2023-05-11 11:41:21 -0700

    a

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
-import pyautogui as p
-import sys
-import time
 
-time.sleep(2)
-for i in range(0,15):
-    p.doubleClick()
-    p.doubleClick()
-print("done")
+import pyautogui as p
+import time
+time.sleep(3)
+
+
+a= p.locateCenterOnScreen(f"assets/shipment.png")
+p.moveTo(a)
