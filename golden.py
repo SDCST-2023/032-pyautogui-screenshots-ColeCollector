dedf3a602e8837f1c52edbe0bd034ef850cf920b
ColeCollector <colen827@deltalearns.ca>
2023-05-11 11:27:20 -0700

    a

--- golden.py
+++ golden.py
@@ -3,17 +3,16 @@ import time
 
 
 def golden():
-    golden2 = p.locateOnScreen('assets/golden2.png', confidence = 0.4)
     golden1 = p.locateOnScreen('assets/golden1.png', confidence = 0.4)
-
-    if golden2 != None:
-        p.moveTo(golden2)
-        p.click()
-        print('golden cookie2')
-
-    elif golden1 != None:
+    golden2 = p.locateOnScreen('assets/golden2.png', confidence = 0.4)
+    
+    if golden1 != None:
         p.moveTo(golden1)
         p.click()
         print('golden cookie1')
 
-            
+
+    elif golden2 != None:
+        p.moveTo(golden2)
+        p.click()
+        print('golden cookie2')
